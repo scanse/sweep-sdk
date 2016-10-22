e7a774f5cddb8291159b863e1763e78c8971888f
Daniel J. Hofmann <daniel@trvx.org>
2016-10-22 13:09:30 -0700

    Buildings

--- sweejs/sweejsd/app.js
+++ sweejs/sweejsd/app.js
@@ -8,7 +8,7 @@ const wss = require('ws').Server({port: 5000});
 
 
 const app = koa();
-app.use(srv('.', {index: 'map.html'}));
+app.use(srv(__dirname, {index: 'map.html'}));
 app.listen(8080);
 
 
@@ -18,11 +18,19 @@ sweep.startScanning();
 
 function pushScanInto(ws) {
   return function push() {
+
     sweep.scan(2000, (err, samples) => {
       if (err) return;
 
+      const latitude = 52.511433;
+      const longitude = 13.389726;
+
       samples.forEach((sample) => {
-        const message = {angle: sample.angle, distance: sample.distance};
+
+        // TODO: convert angle+distance to height => sin(radians(angle)) * distance
+        // TODO: convert angle+distance to distance-to-left/right => cos(radians(angle)) * distance
+
+        const message = {latitude: latitude, longitude: longitude, angle: sample.angle, distance: sample.distance};
         ws.send(JSON.stringify(message), error => {/**/});
       });
     });
@@ -32,3 +40,5 @@ function pushScanInto(ws) {
 wss.on('connection', ws => {
   setInterval(pushScanInto(ws), 2000);
 });
+
+console.log('Now open browser at http://localhost:8080')
