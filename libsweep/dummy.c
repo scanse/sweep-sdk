0e52c5f6c27bb71c506cfc6acbf79a874efd08a9
Daniel J. Hofmann <daniel@trvx.org>
2016-11-10 22:43:28 +0100

    Dummy Reports 2m

--- libsweep/dummy.c
+++ libsweep/dummy.c
@@ -143,7 +143,7 @@ int32_t sweep_scan_get_distance(sweep_scan_s scan, int32_t sample) {
   SWEEP_ASSERT(scan);
   SWEEP_ASSERT(sample >= 0 && sample < scan->count && "sample index out of bounds");
 
-  return 20;
+  return 2 * 100; // 2 meter
 }
 
 int32_t sweep_scan_get_signal_strength(sweep_scan_s scan, int32_t sample) {
