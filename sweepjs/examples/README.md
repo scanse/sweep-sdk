853f113543621daf6ce8553177f7d2153d2657c4
Daniel J. Hofmann <daniel@trvx.org>
2016-11-12 11:02:30 +0100

    Removes Half-Finished Example

---
+++ sweepjs/examples/README.md
@@ -0,0 +1,10 @@
+# SweepJs Examples
+
+Requires `libsweep.so` to be installed.
+Requires the SweepJs module to be installed.
+
+### License
+
+Copyright © 2016 Daniel J. Hofmann
+
+Distributed under the MIT License (MIT).
---
+++ sweepjs/examples/websocket/sweepjsd.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const sweepjs = require('../..');
+
+const koa = require('koa');
+const srv = require('koa-static');
+const wss = require('ws').Server({port: 5000});
+
+
+const app = koa();
+app.use(srv(__dirname, {index: 'index.html'}));
+app.listen(8080);
+
+
+const sweep = new sweepjs.Sweep();
+sweep.startScanning();
+
+
+function pushScanInto(ws) {
+  return function push() {
+
+    sweep.scan((err, samples) => {
+      if (err) return;
+
+      samples.forEach((sample) => {
+        // x = cos(angle) * distance
+        // y = sin(angle) * distance
+        const message = {angle: sample.angle, distance: sample.distance};
+        ws.send(JSON.stringify(message), error => {/**/});
+      });
+    });
+  };
+}
+
+wss.on('connection', ws => {
+  setInterval(pushScanInto(ws), 2000);
+});
+
+console.log('Now open browser at http://localhost:8080')
