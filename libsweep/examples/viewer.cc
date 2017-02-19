8b8c9be5bab66e82fdc8b73ba6c1e31814da2fb9
Daniel J. Hofmann <daniel@trvx.org>
2017-02-20 00:16:03 +0100

    Close Window on ESC in Viewer

--- libsweep/examples/viewer.cc
+++ libsweep/examples/viewer.cc
@@ -42,6 +42,9 @@ int main() try {
       while (window->pollEvent(event)) {
         if (event.type == sf::Event::Closed)
           window->close();
+        else if (event.type == sf::Event::KeyPressed)
+          if (event.key.code == sf::Keyboard::Escape)
+            window->close();
       }
 
       window->clear(kColorCream);
