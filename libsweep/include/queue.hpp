c6b2dccf484fff310b212be6e3852e20d635127b
MikeGitb <MikeGitb@users.noreply.github.com>
2017-05-09 00:57:49 +0200

    [libsweep] Forward errors from accumulate_scans to get_scan

--- libsweep/include/queue.hpp
+++ libsweep/include/queue.hpp
@@ -10,12 +10,17 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <exception>
 #include <mutex>
 #include <queue>
 #include <utility>
 
 namespace sweep {
 namespace queue {
+
+struct ReadCanceled : std::exception {
+  const char* what() const noexcept override { return "Read was canceled by user"; }
+};
 
 template <typename T> class queue {
 public:
@@ -30,39 +35,55 @@ public:
   }
 
   // Add an element to the queue.
-  void enqueue(T v) {
-    std::lock_guard<std::mutex> lock(the_mutex);
+  void push(T v) {
+    {
+      std::lock_guard<std::mutex> lock(the_mutex);
 
-    // if necessary, remove the oldest scan to make room for new
-    if (static_cast<int32_t>(the_queue.size()) >= max_size)
-      the_queue.pop();
+      // if necessary, remove the oldest scan to make room for new
+      if (static_cast<int32_t>(the_queue.size()) >= max_size)
+        the_queue.pop();
 
-    the_queue.push(v);
+      the_queue.push(v);
+    }
     the_cond_var.notify_one();
   }
 
   // If the queue is empty, wait till an element is avaiable.
-  T dequeue() {
+  T pop() {
     std::unique_lock<std::mutex> lock(the_mutex);
     // wait until queue is not empty
-    while (the_queue.empty()) {
-      // the_cond_var could wake up the thread spontaneously, even if the queue is still empty...
-      // so put this wakeup inside a while loop, such that the empty check is performed whenever it wakes up
-      the_cond_var.wait(lock); // release lock as long as the wait and reaquire it afterwards.
+    the_cond_var.wait(lock, [this] { return error || !the_queue.empty(); });
+
+    if (error) {
+      auto t = error;
+      error = t;
+      std::rethrow_exception(error);
     }
+
     auto v = the_queue.front();
     the_queue.pop();
     return v;
   }
 
+  void push_error(std::exception_ptr eptr) {
+    {
+      std::lock_guard<std::mutex> lock(the_mutex);
+      error = eptr;
+    }
+    the_cond_var.notify_one();
+  }
+
+  void cancel_read() { push_error(ReadCanceled{}); }
+
 private:
+  std::exception_ptr error;
   int32_t max_size;
   std::queue<T> the_queue;
   mutable std::mutex the_mutex;
   mutable std::condition_variable the_cond_var;
 };
 
-} // ns queue
-} // ns sweep
+} // namespace queue
+} // namespace sweep
 
 #endif
