7a5f2fc0c7519c20c488a3c5f627dbdae38d034b
Kenzie Togami <ket1999@gmail.com>
2017-08-16 00:59:06 -0700

    Update example to match others

--- jsweep/src/test/java/io/scanse/sweep/Example.java
+++ jsweep/src/test/java/io/scanse/sweep/Example.java
@@ -6,10 +6,13 @@ public class Example {
 
     public static void main(String[] args) throws Exception {
         try (SweepDevice device = new SweepDevice("/dev/ttyUSB0")) {
+            int speed = device.getMotorSpeed();
+            int rate = device.getSampleRate();
+
+            System.out.println(String.format("Motor Speed: %s Hz", speed));
+            System.out.println(String.format("Sample Rate: %s Hz", rate));
+
             device.startScanning();
-            while (!device.isMotorReady()) {
-                Thread.sleep(10);
-            }
 
             for (List<SweepSample> s : device.scans()) {
                 System.err.println(s);
